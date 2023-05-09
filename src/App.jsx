b4bca4c0d1aa1ea7056ae70ee4643b922f57f76d
Shaif Uddin Ahamed <shaifuddin.ahamed@gmail.com>
2023-05-09 16:16:52 +0600

    45.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const products = [
-    {
-      id: 1,
-      name: "Laptop",
-      price: 1000,
-    },
-    {
-      id: 2,
-      name: "Smartphone",
-      price: 2000,
-    },
-    {
-      id: 3,
-      name: "Tablet",
-      price: 3000,
-    },
-  ];
   return (
     <>
-      {products.map((product) => (
-        <Product key={product.id} name={product.name} price={product.price} />
-      ))}
+      <Counter></Counter>
     </>
   );
 }
-
-function Product(props) {
-  console.log(props);
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+  // console.log(abc);
   return (
-    <div className="product">
-      <h3>Name: {props.name}</h3>
-      <p>Price: {props.price}</p>
-    </div>
+    <>
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+    </>
   );
 }
 
